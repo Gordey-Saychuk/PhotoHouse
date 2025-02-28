cfca3e8f2d8e537b0ab1048958c13d16b9223f4a
Daniil <eddanya@gmail.com>
2025-02-28 17:38:39 +0300

    html css

--- components/SecondSlider/index.vue
+++ components/SecondSlider/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import {useCartStore} from "~/store/useCart";
+
 const breakpoints = ref({
   1: {
     slidesPerView: 4.15,
@@ -14,7 +16,10 @@ defineProps({
     type: Array,
   }
 })
-
+const store = useCartStore()
+const loadPhoto = (photo) => {
+  store.initCart(photo)
+}
 const containerRef = ref(null)
 const swiper = useSwiper(containerRef, {
   breakpoints: breakpoints.value,
@@ -32,9 +37,10 @@ const swiper = useSwiper(containerRef, {
               v-for="(slide, idx) in slides"
               :key="idx"
           >
-            <a class="relative" :href="slide.icon" data-fancybox="gallery">
+            <button class="relative">
               <img class="h-32 object-cover rounded-xl" :src="slide.icon" alt="slide.icon">
-            </a>
+              <UButton  @click.prevent="loadPhoto(slide.icon)">выбрать</UButton>
+            </button>
           </swiper-slide>
         </swiper-container>
       </ClientOnly>
--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,35 @@ import {useCartStore} from "~/store/useCart";
 
 const ready = ref(true)
 const store = useCartStore()
-const loadPhoto = () => {
-  store.initCart('wadawd')
+const file = ref()
+const getPhoto = (e) => {
+  console.log(e.target.files[0])
 }
+watch(() => store.cart,() => {
+  store.cart ? ready.value = false : ready.value = true
+})
 </script>
 
 <template>
   <main>
     <section class="mb-5 relative">
+      {{ready}}
       <UContainer>
-        <img class="w-full mb-2" src="/assets/images/hero.webp" alt="hero">
+        <img v-if="!store.cart" class="w-full mb-2" src="/assets/images/hero.webp" alt="hero">
+        <div class="w-full h-72 mb-2 dark:bg-zinc-800 rounded-xl" v-else>
+          <img  class="w-full h-full object-contain mb-2" :src="store.cart" alt="hero">
+        </div>
         <UButton to="/catalog" class="mb-5" >Выбрать в каталоге примеров</UButton>
         <img class="w-full mb-2 cursor-pointer" src="/assets/images/heroBottom.webp" alt="heroBottom">
-        <UButton @click.prevent="loadPhoto" type="file" class="mb-5" >Загрузить фото с одеждой</UButton>
+        <div v-if="false" class="w-full h-72 mb-2 dark:bg-zinc-800 rounded-xl">
+          <img  class="w-full h-full object-contain mb-2" :src="store.cart" alt="hero">
+        </div>
+        <label class="flex mb-5 cursor-pointer">
+          <input @change="getPhoto" type="file" hidden>
+          <UButton  class=" pointer-events-none" >Загрузить фото с одеждой
+          </UButton>
+        </label>
         <UButton :disabled="ready" to="/catalog" :class="{'dark:bg-zinc-800': ready, 'dark:text-zinc-500': ready, 'bg-neutral-200': ready, 'text-zinc-400': ready}" >Продолжить</UButton>
-        {{store.cart}}
       </UContainer>
     </section>
   </main>
